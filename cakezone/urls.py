bb44e678f352b5b172df84ad6c70a439d7ac2003
Roma270719 <roma.bokach.670@gmail.com>
2026-06-19 06:08:16 +0200

    .

--- cakezone/urls.py
+++ cakezone/urls.py
@@ -38,6 +38,4 @@ urlpatterns = [
     path('logout/', user_logout, name='logout'),
 ]
 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
